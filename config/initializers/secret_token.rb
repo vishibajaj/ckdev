81e1ccfaba8ac04561c39e57f5114b17b7cf2745
vishibajaj <visheshbajaj@geobeats.com>
2014-08-01 13:09:24 +0530

    created first ideas view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ckdev::Application.config.secret_token = '2d69c41d14a84b3a00cbece0e0bc868a0e13eef553d14163e39a250ce9524d3bb43fda37cce13e92ce5783504cb0016255298b56e71a21e34a85d748463e7e0c'
